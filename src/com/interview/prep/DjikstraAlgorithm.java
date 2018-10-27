72ed95841c3552d1b21f2c615a680f0a7b688c27
Yokesh V S <yokeshvsk@gmail.com>
2018-10-27 12:20:05 -0500

    Prep updates

--- src/com/interview/prep/DjikstraAlgorithm.java
+++ src/com/interview/prep/DjikstraAlgorithm.java
@@ -1,10 +1,14 @@
 package com.interview.prep;
 
-class GraphNode {
-	int val;
-	GraphNode next;
+import java.util.HashMap;
+import java.util.Map;
 
-	public GraphNode(int x) {
+class GraphNode {
+	String val;
+	GraphNode next;
+	Map<GraphNode, Integer> neighbours = new HashMap<>();
+
+	public GraphNode(String x) {
 		this.val = x;
 	}
 }
@@ -12,7 +16,29 @@ class GraphNode {
 public class DjikstraAlgorithm {
 
 	public static void main(String[] args) {
-		
+		GraphNode aNode = new GraphNode("A");
+		GraphNode bNode = new GraphNode("B");
+		GraphNode cNode = new GraphNode("C");
+		GraphNode dNode = new GraphNode("D");
+		GraphNode eNode = new GraphNode("E");
+		aNode.neighbours.put(bNode, 6);
+		aNode.neighbours.put(dNode, 1);
+		dNode.neighbours.put(aNode, 1);
+		dNode.neighbours.put(bNode, 2);
+		dNode.neighbours.put(eNode, 1);
+		bNode.neighbours.put(dNode, 2);
+		bNode.neighbours.put(eNode, 2);
+		bNode.neighbours.put(cNode, 5);
+		eNode.neighbours.put(dNode, 1);
+		eNode.neighbours.put(bNode, 2);
+		eNode.neighbours.put(cNode, 5);
+
+		System.out.println("Shortest distance between A and C: " + getShortestDistance(aNode, cNode));
+	}
+
+	public static Integer getShortestDistance(GraphNode node1, GraphNode node2) {
+		return null;
+
 	}
 
 }
--- src/com/interview/prep/MergeSort.java
+++ src/com/interview/prep/MergeSort.java
@@ -1,19 +1,64 @@
 package com.interview.prep;
 
 public class MergeSort {
+	private static int[] arr = { 10, 40, 5, 100, 20, 50 };
 
 	public static void main(String[] args) {
-		int[] arr = { 10, 40, 5, 100, 20, 50 };
-		int[] result = mergeSort(arr,0,arr.length);
-		for (int i = 0; i < result.length; i++) {
-			System.out.print(result[i]);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(" " + arr[i]);
+		}
+		mergeSort(arr, 0, arr.length - 1);
+		System.out.println();
+		System.out.println("After sort");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(" " + arr[i]);
 		}
 	}
-	
-	public static int[] mergeSort(int[] arr, int start, int end) {
-		int mid = end/2;
-		int[] l1 = mergeSort(arr, start, mid-1);
-		int[] l2 = mergeSort(arr, mid, end);
-		return new int[0];
+
+	public static void mergeSort(int[] arr, int start, int end) {
+		if (start < end) {
+			int mid = (start + end) / 2;
+			mergeSort(arr, start, mid);
+			mergeSort(arr, mid + 1, end);
+			merge(arr, start, mid, end);
+		}
+	}
+
+	public static void merge(int[] arr, int start, int mid, int end) {
+		int n1 = mid - start + 1;
+		int n2 = end - mid;
+		System.out.println("n1: " + n1);
+		System.out.println("n2: " + n2);
+		int[] leftArray = new int[n1];
+		int[] rightArray = new int[n2];
+
+		for (int i = 0; i < n1; i++) {
+			leftArray[i] = arr[start + i];
+		}
+
+		for (int i = 0; i < n2; i++) {
+			rightArray[i] = arr[mid + 1 + i];
+		}
+
+		int i = 0, j = 0, k = start;
+		while (i < n1 && j < n2) {
+			if (leftArray[i] <= rightArray[j]) {
+				arr[k] = leftArray[i];
+				i++;
+			} else {
+				arr[k] = rightArray[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k++] = leftArray[i++];
+		}
+
+		while (j < n2) {
+			arr[k++] = rightArray[j++];
+		}
+
 	}
 }
